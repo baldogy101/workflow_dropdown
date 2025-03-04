8915713cfbfaf512c68d93adcb654267bc1d4616
baldogy101 <baldogy01@gmail.com>
2025-03-04 17:31:29 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    loadComponent("components/header.html", "header");
+    loadComponent("components/main.html", "main-content");
+    loadComponent("components/footer.html", "footer");
+});
